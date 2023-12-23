8e9c8ba419ae0f31da2171c1d8790ea98c7db4fd
Quan <le.quan@outlook.com>
2023-12-23 10:26:13 +0700

    logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { CatsController } from "./cats/cats.controller";
-import { CatsService } from "./cats/cats.service";
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService]
+  imports: [AuthModule, CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('auth');
+  }
 }
